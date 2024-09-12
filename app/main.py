44cbecd7294210e926e29b207f6acb0a906fd73c
Douglas Landvik <douglas.landvik@gmail.com>
2024-09-12 16:45:04 +0200

    fix(skillrating): add rank to default also

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Add the URL of your frontend
+    allow_origins=["http://localhost:3000", "https://terminology-hub.vercel.app"],  # Add the URL of your frontend
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
