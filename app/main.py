d94beeff79847e724657de2abcf6a7cd34bb57b3
Douglas Landvik <douglas.landvik@gmail.com>
2024-09-12 13:11:37 +0200

    fix(skillrating): add rank to default also

--- app/main.py
+++ app/main.py
@@ -3,9 +3,20 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List
 import sqlite3
+from fastapi.middleware.cors import CORSMiddleware
+
 
 # Initialize FastAPI app
 app = FastAPI()
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Your frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
 
 # Function to get a database connection
 def get_db_connection():
